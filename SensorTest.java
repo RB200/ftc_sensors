bfabf766439a6ce56fca3b05bca104f4eee0ebe8
Ryan Beaulieu <66707944+RB200@users.noreply.github.com>
2024-10-02 11:33:57 -0700

    Update SensorTest.java

--- SensorTest.java
+++ SensorTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-//@TeleOp(name= "Distance Sensor Test")
+@TeleOp(name= "Distance Sensor Test")
 public class SensorTest extends LinearOpMode {
     private LaserRangeSensor rangeSensor;
 
